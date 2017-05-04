3d678c89633a5b8f845df6654929d3cdb4b9f594
Damian Kaye <damian.r.kaye@gmail.com>
2017-05-04 16:24:36 +0200

    Adjust main.go + add config for items from json storage

--- main.go
+++ main.go
@@ -1,55 +1,19 @@
 package main
 
 import (
-	"crypto/rand"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/big"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"personal/chest-item-generator/model"
+	"personal/chest-item-generator/service"
 )
-
-var ItemMap = make(map[string][]Item)
-
-const (
-	COMMON     = "common"
-	RARE       = "rare"
-	SUPER_RARE = "super"
-	ULTRA_RARE = "ultra"
-	ANCIENT    = "ancient"
-)
-
-type Item struct {
-	Identifier  int64  `json:"identifier"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Rarity      string `json:"rarity"`
-	MaxQty      int    `json:"maxQty"`
-}
-
-type ItemGroup struct {
-	Item     Item  `json:"item"`
-	Quantity int64 `json:"quantity"`
-}
-
-func (itemGroup ItemGroup) String() string {
-	return fmt.Sprintf("Item Name: %s, Max Quantity: %d", itemGroup.Item.Name, itemGroup.Item.MaxQty)
-}
-
-func (item Item) String() string {
-	return fmt.Sprintf("Item Name: %s, Max Quantity: %d", item.Name, item.MaxQty)
-}
-
-func getRandomNumber(max int64) int64 {
-	number, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
-
-	if err != nil {
-		println(err)
-	}
-
-	return number.Int64()
-}
 
 func getenv(key, fallback string) string {
 	value := os.Getenv(key)
@@ -57,24 +21,6 @@ func getenv(key, fallback string) string {
 		return fallback
 	}
 	return value
-}
-
-func getRareItem(rarity string) Item {
-	items := ItemMap[rarity]
-	return items[getRandomNumber(int64(len(items)))]
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		var chestItems []ItemGroup
-
-		chestItems = append(chestItems, rollForCommonItems()...)
-		chestItems = append(chestItems, rollForRareItems()...)
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(chestItems)
-	}
 }
 
 func main() {
@@ -85,48 +31,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	json.Unmarshal(file, &ItemMap)
+	itemMap := make(map[string][]model.Item)
+	json.Unmarshal(file, &itemMap)
 
-	http.HandleFunc("/chest_items", handler)
-	http.ListenAndServe(fmt.Sprintf(":%s", getenv("PORT", "8080")), nil)
-}
+	config := &service.Config{
+		ItemMap: itemMap,
+	}
 
-func rollForCommonItems() []ItemGroup {
-	var commonChestItems []ItemGroup
+	rootRouter := service.Routes(config)
 
-	commonItems := ItemMap[COMMON]
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: rootRouter,
+	}
 
-	for _, item := range commonItems {
-		quantity := getRandomNumber(int64(item.MaxQty))
-		if quantity == 0 {
-			continue
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
 		}
-		itemGroup := ItemGroup{Item: item, Quantity: quantity}
-		commonChestItems = append(commonChestItems, itemGroup)
+	}()
+
+	<-interrupt
+	deadline := time.Now().Add(3 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
 	}
 
-	return commonChestItems
-}
-
-func rollForRareItems() []ItemGroup {
-	diceRoll := getRandomNumber(100)
-
-	var rareItem Item
-
-	switch {
-	case diceRoll > 65 && diceRoll <= 85:
-		rareItem = getRareItem(RARE)
-	case diceRoll > 85 && diceRoll <= 95:
-		rareItem = getRareItem(SUPER_RARE)
-	case diceRoll > 95 && diceRoll <= 99:
-		rareItem = getRareItem(ULTRA_RARE)
-	case diceRoll == 100:
-		rareItem = getRareItem(ANCIENT)
-	}
-
-	if rareItem != (Item{}) {
-		return []ItemGroup{{Item: rareItem, Quantity: 1}}
-	}
-
-	return []ItemGroup{}
 }
---
+++ service/config.go
@@ -0,0 +1,7 @@
+package service
+
+import "personal/chest-item-generator/model"
+
+type Config struct {
+	ItemMap map[string][]model.Item
+}
